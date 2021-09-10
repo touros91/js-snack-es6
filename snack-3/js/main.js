d47c4fbdadb04cbfccfcde0816722a643bcec31e
touros91 <andreageraci91@gmail.com>
2021-09-10 15:14:09 +0200

    Aggiunto validazione

--- snack-3/js/main.js
+++ snack-3/js/main.js
@@ -4,12 +4,15 @@
 // Usiamo i nuovi metodi degli array foreach o filter.
 
 function arraySelection (array, num1, num2){
+    while (isNaN(num1) || num1 >= num2 || num1 < 0 || num1 >= array.length - 1 || isNaN(num2) || num2 > array.length - 1) {
+        return false;
+    }
     const newArray = array.filter((element, index) => index >= num1 && index <= num2);
     return newArray;
 }
 
-const arr = [1, 2, 3, 4, 5];
-let numStart = 1;
-let numEnd = 3;
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let numStart = 3;
+let numEnd = 6;
 
 console.log(arraySelection(arr, numStart, numEnd));
